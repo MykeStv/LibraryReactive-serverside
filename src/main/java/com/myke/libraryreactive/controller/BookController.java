131e6cc15185630b69baa9c3aefaeffc8d82e288
MykeStv <mykestv22@gmail.com>
2022-04-06 12:22:09 -0500

    controller implementado

---
+++ src/main/java/com/myke/libraryreactive/controller/BookController.java
@@ -0,0 +1,50 @@
+package com.myke.libraryreactive.controller;
+
+import com.myke.libraryreactive.model.Book;
+import com.myke.libraryreactive.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public Flux<Book> getBooks() {
+        return this.bookService.getBooks();
+    }
+
+    @GetMapping(path = "/{id}")
+    private Mono<Book> findBookId(@PathVariable("id") String id) {
+        return this.bookService.getBookById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Book> findBookId(@RequestBody Book book) {
+        return this.bookService.saveBook(book);
+    }
+
+    @PutMapping(path = "/{id}")
+    private Mono<ResponseEntity<Book>> updateBook(@PathVariable("id") String id, @RequestBody Book book) {
+        return this.bookService.updateBook(id, book)
+                .flatMap(b -> Mono.just(ResponseEntity.ok(b)))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
+
+    @DeleteMapping(path = "/{id}/delete")
+    private Mono<ResponseEntity<Book>> deleteBook(@PathVariable("id") String id) {
+        return this.bookService.deleteBook(id)
+                .flatMap(b -> Mono.just(ResponseEntity.ok(b)))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
+
+
+}
--- src/main/java/com/myke/libraryreactive/repository/BookRepository.java
+++ src/main/java/com/myke/libraryreactive/repository/BookRepository.java
@@ -3,8 +3,9 @@ package com.myke.libraryreactive.repository;
 import com.myke.libraryreactive.model.Book;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface BookRepository extends ReactiveMongoRepository<Book, String> {
-
+    Mono<Book> findByName(String name);
 }
